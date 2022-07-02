f49bafb4ef194699d3a5567eccd7f0108b7e78d0
David Lin <dlin2028@gmail.com>
2022-07-01 22:08:47 -0700

    I AM A DATA MANGLING GOD

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import pandas as pd
+import csv
 
-listing_titles = pd.read_csv('./dataset/Listing_Titles.tsv', sep='\t')
-tagged_titles = pd.read_csv('./dataset/Train_Tagged_Titles.tsv', on_bad_lines='skip')
+def strip(text):
+    try:
+        return text.strip()
+    except AttributeError:
+        return text
+
+listing_titles = pd.read_csv('./testset/Listing_Titles.tsv', sep='\t', on_bad_lines='skip', quoting=csv.QUOTE_NONE, encoding='utf8')
+
+tagged_titles = pd.read_csv('./testset/Train_Tagged_Titles.tsv', sep='\t', on_bad_lines='skip', quoting=csv.QUOTE_NONE, encoding='utf8')
 
 tags = {'accents', 'brand', 'character', 'character family', 'closure', 'color', 'country/region of manufacture', 'department', 'fabric type', 'features', 'handle drop', 'handle style', 'handle/strap material', 'hardware material', 'lining material', 'mpn', 'material', 'measurement, dimension', 'model', 'occasion', 'pattern', 'pocket type', 'product line', 'season', 'size', 'strap drop', 'style', 'theme', 'trim material', 'type'}
 
+print(listing_titles)
+print(tagged_titles)
+print(tagged_titles.groupby('Tag')['Token'].apply(list)[0:10])
+
+# quit
+
+# df2 = tagged_titles[0:100].groupby(['Tag']).sum()
 
 
-print(tagged_titles[97:104])
+# print(tagged_titles[97:104])
+# print("--------------------")
+# print(df2[0:10])
