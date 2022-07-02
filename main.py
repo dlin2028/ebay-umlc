62da6a6d558a81b7f7b21b34e9ae1cc464f978d4
David Lin <dlin2028@gmail.com>
2022-07-01 21:39:59 -0700

    add tags

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 listing_titles = pd.read_csv('./dataset/Listing_Titles.tsv', sep='\t')
 tagged_titles = pd.read_csv('./dataset/Train_Tagged_Titles.tsv', on_bad_lines='skip')
 
-tags = {'accents', 'brand', 'character', }
+tags = {'accents', 'brand', 'character', 'character family', 'closure', 'color', 'country/region of manufacture', 'department', 'fabric type', 'features', 'handle drop', 'handle style', 'handle/strap material', 'hardware material', 'lining material', 'mpn', 'material', 'measurement, dimension', 'model', 'occasion', 'pattern', 'pocket type', 'product line', 'season', 'size', 'strap drop', 'style', 'theme', 'trim material', 'type'}
 
 
 
