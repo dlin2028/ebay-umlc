15c989c149b78ca641e484b02bfb8bd034ce40bb
David Lin <dlin2028@gmail.com>
2022-07-01 21:31:37 -0700

    basic remove trash

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import pandas as pd
+
 listing_titles = pd.read_csv('./dataset/Listing_Titles.tsv', sep='\t')
-print(listing_titles[0:10])
+tagged_titles = pd.read_csv('./dataset/Train_Tagged_Titles.tsv', on_bad_lines='skip')
+
+tags = {'accents', 'brand', 'character', }
+
+
+
+print(tagged_titles[97:104])
---
+++ pain.py
@@ -0,0 +1,26 @@
+
+# this is actually painful
+# dtypes = {
+#     "Accents": "category",
+#     "Brand": "category",
+#     "Character": "category",
+#     "Character Family": "category",
+#     "Color": "category",
+#     "Country/Region of Manufacture": "category",
+#     "Department": "category",
+#     "Fabric Type": "category",
+#     "Features": "category",
+#     "Handle Drop": "category",
+#     "Handle Style": "category",
+#     "Handle/Strap Material ": "category",
+#     "Lining Material": "category",
+#     "MPN": "category",
+#     "Material": "category",
+#     "MPN": "category",
+#     "Lining Material": "category",
+#     "Measurement, dimension": "category",
+#     "Model": "category",
+#     "Occasion": "category",
+#     "Pattern": "category",
+#     "Pocket Type": "category",
+# }
