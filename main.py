22c3a471edaaa1ce9818fabf2ac1ee1557c740cc
David Lin <dlin2028@gmail.com>
2022-07-11 22:45:57 -0700

    added glutes and milkshakes

--- main.py
+++ main.py
@@ -9,14 +9,33 @@ tagged_titles = pd.read_csv('./testset/Train_Tagged_Titles.tsv', sep='\t', on_ba
 
 tags = ['Accents', 'Brand', 'Character', 'Character Family', 'Closure', 'Color', 'Country/Region of Manufacture', 'Department', 'Fabric Type', 'Features', 'Handle Drop', 'Handle Style', 'Handle/Strap Material', 'Hardware Material', 'Lining Material', 'MPN', 'Material', 'Measurement, Dimension', 'Model', 'Occasion', 'Pattern', 'Pocket Type', 'Product Line', 'Season', 'Size', 'Strap Drop', 'Style', 'Theme', 'Trim Material', 'Type']
 
-series = tagged_titles.groupby('Record Number')['Token'].apply(list)
+tiddies = tagged_titles.groupby('Record Number')['Token'].apply(list).to_dict()
+ass = tagged_titles.groupby('Record Number')['Tag'].apply(list).to_dict()
 
-print(series)
+print(tiddies[1])
+print("<3<3 wuwuwuwuuw I LOVE AMOURANTH DSLKFJSLDKFJSDLK:FJ")
+print(ass[1])
+
+amouranth = [[(tiddies[i][tiddie], ass[i][tiddie]) for tiddie in range(0, len(ass[i]))] for i in range(1, len(ass) + 1)]
+
+# [[(LOUIS, Brand), (VITTON, Brand)],[(LOUIS, Brand), (VITTON, Brand)...
+
+#print(tiddies) #SHOW ME THE TIDDIES
+print("tiddies")
+print(amouranth)
+
+# ['LOUIS', 'VUITTON', 'M40096', 'Handbag', 'Priscilla', 'Multi-color', 'canvas', 'Multi-color', 'canvas']
+# <3<3 wuwuwuwuuw I LOVE AMOURANTH DSLKFJSLDKFJSDLK:FJ
+# ['Brand', nan, 'MPN', 'Type', 'Model', 'Color', 'Fabric Type', 'Color', 'Fabric Type']
+# tiddies
+
+#tokensntags = [(tokens[str(i)], tags[str(i)]) for i in range(0, len(tokens))]
+
 
 quit()
 
 
-clean_dic = {k: series[k] for k in tags if k in series}
+clean_dic = {k: series[k] for k in ass if k in series}
 #python moment
 clean_dic = {item : list(set({x.lower() for x in value })) for (item, value) in clean_dic.items()}
 
@@ -30,15 +49,15 @@ def find_tag(token):
 listing_titles = listing_titles.to_dict()
 titles = listing_titles['Title']
 #titles = {num : vals.split(" ") for (num, vals) in titles.items()}
-iamagod = {k : {find_tag(token.lower()) for token in title} for (k, title) in titles.items()}
+amouranth = {k : {find_tag(token.lower()) for token in title} for (k, title) in titles.items()}
 
-print(iamagod)
+print(amouranth)
 print(titles)
 
 output = ""
-for i in range(len(iamagod)):
-    for j in range(len(list(iamagod.values())[i])):
-        output += str(i) + " " + list(list(titles.values())[i])[j] + " " + list(list(iamagod.values())[i])[j] + "\n"
+for i in range(len(amouranth)):
+    for j in range(len(list(amouranth.values())[i])):
+        output += str(i) + " " + list(list(titles.values())[i])[j] + " " + list(list(amouranth.values())[i])[j] + "\n"
 
 f = open("output.txt", "w")
 f.write(output)
