bb464363d3abf61e89663eb8acc25d3696bda3e5
David Lin <dlin2028@gmail.com>
2022-07-18 22:23:15 -0700

    POOP on the competition

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+from cgitb import text
 import spacy
 import csv
 import pandas as pd
@@ -17,20 +18,30 @@ if __name__ == '__main__':
     listing_titles = pd.read_csv('./dataset/Listing_Titles.tsv', sep='\t', on_bad_lines='skip', quoting=csv.QUOTE_NONE, encoding='utf8')
 
     outsize = 1024 * 1024 * 1024 # 1GB
-    output = open("output.txt", "w", encoding="utf-8")  # write mode
 
-    with tqdm(total = 19999921) as pbar:
+    listing_titles = [(listing_titles["Title"][i], i) for i in range(0, len(listing_titles["Title"]))][5003:30002]
+
+    result = []
+
+    with tqdm(total = 25000) as pbar:
         buffer = ""
-        for i, doc in enumerate(nlp.pipe(listing_titles["Title"].to_numpy())):
+        for doc, i in nlp.pipe(listing_titles, as_tuples=True, batch_size=10000):
             for ent in doc.ents:
-                buffer += str(i) + ' ' + ent.text + ' ' + ent.label_ + '\n'
+                result.append(str(i) + '\t' + ent.text + '\t' + ent.label_)
             pbar.update(1)
-            if(len(buffer) > outsize):
-                output.write(buffer)
-                buffer = ""
             
-    output.write(buffer)
+    output = open("output.txt", "w", encoding="utf-8")  # write mode
+    output.write('\n'.join(result))
     output.close()
+
+quit()
+
+
+
+
+
+
+
 
 
 # outsize = 1024 * 1024 * 1024 # 1GB
---
+++ test_output.py
@@ -0,0 +1,4 @@
+
+
+output = open("output.txt", "r", encoding="utf-8")  # write mode
+print(output.read(50))
